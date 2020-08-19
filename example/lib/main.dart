85809aab6f8dab86461e9e51771687cca1d7dedc
Britannio <britanniojarrett@gmail.com>
2020-08-19 11:15:28 +0100

    add availability status to the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:in_app_review/in_app_review.dart';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +12,26 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final InAppReview _inAppReview = InAppReview.instance;
   String _appStoreId = '';
+  bool _isAvailable;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (Platform.isIOS || Platform.isMacOS) {
+        _inAppReview.isAvailable().then((bool isAvailable) {
+          setState(() {
+            _isAvailable = isAvailable;
+          });
+        });
+      } else {
+        setState(() {
+          _isAvailable = false;
+        });
+      }
+    });
+  }
 
   void _setAppStoreId(String id) => _appStoreId = id;
 
@@ -25,12 +46,19 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    const loadingMessage = 'LOADING';
+    const availableMessage = 'AVAILABLE';
+    const unavailableMessage = 'UNAVAILABLE';
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('InAppReview Example')),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text(
+              'InAppReview status: ${_isAvailable == null ? loadingMessage : _isAvailable ? availableMessage : unavailableMessage}',
+            ),
             TextField(
               onChanged: _setAppStoreId,
               decoration: InputDecoration(hintText: 'App Store ID'),
