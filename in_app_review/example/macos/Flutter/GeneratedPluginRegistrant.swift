a6f27d0587e33ce082c13038191c83d3db405bab
Britannio <britanniojarrett@gmail.com>
2021-02-25 19:51:27 +0000

    [in_app_review] add default_package definition to pubspec.yaml

--- in_app_review/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ in_app_review/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -2,15 +2,11 @@
 //  Generated file. Do not edit.
 //
 
-// clang-format off
-
 import FlutterMacOS
 import Foundation
 
-import in_app_review
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
