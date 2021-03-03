3c6ad9826b6812f44780ac178ca5a9c2e21cdb5b
Britannio <britanniojarrett@gmail.com>
2021-03-03 13:40:59 +0000

    [in_app_review] add logging to iOS plugin

--- in_app_review/ios/Classes/SwiftInAppReviewPlugin.swift
+++ in_app_review/ios/Classes/SwiftInAppReviewPlugin.swift
@@ -9,19 +9,28 @@ public class SwiftInAppReviewPlugin: NSObject, FlutterPlugin {
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
     
+    private func logMessage(message: String) {
+        print("SwiftInAppReviewPlugin: " + message)
+    }
+    
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        logMessage(message: "handle: " + call.method)
         
         switch call.method {
         case "requestReview":
             //App Store Review
             if #available(iOS 14.0, *) {
+                logMessage(message: "iOS 14+")
                 if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                    logMessage(message: "scene found")
                     SKStoreReviewController.requestReview(in: scene)
                     result(nil)
                 } else {
+                    logMessage(message: "scene not found")
                     result(FlutterError(code: "no-scene", message: "In-App Review could not find active scene", details: nil))
                 }
             } else if #available(iOS 10.3, *) {
+                logMessage(message: "iOS 10.3+")
                 SKStoreReviewController.requestReview()
                 result(nil)
             } else {
@@ -29,8 +38,10 @@ public class SwiftInAppReviewPlugin: NSObject, FlutterPlugin {
             }
         case "isAvailable":
             if #available(iOS 10.3, *) {
+                logMessage(message: "available")
                 result(true)
             } else {
+                logMessage(message: "unavailable")
                 result(false)
             }
         case "openStoreListing":
@@ -49,6 +60,7 @@ public class SwiftInAppReviewPlugin: NSObject, FlutterPlugin {
                 result(nil)
             };
         default:
+            logMessage(message: "method not implemented")
             result(FlutterMethodNotImplemented)
         }
     }
