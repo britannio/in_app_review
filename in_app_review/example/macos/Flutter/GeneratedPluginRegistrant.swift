1aef22b1033a75029ca36a69a34029efae03981b
Britannio <britanniojarrett@gmail.com>
2021-03-05 11:22:41 +0000

    [in_app_review] fix MacOS MissingPluginException remove dependency overrides from example app to make pub.green happy

--- in_app_review/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ in_app_review/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import in_app_review
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
